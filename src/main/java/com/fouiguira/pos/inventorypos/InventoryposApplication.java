1b4d0cf3ca56bb9fed9316087fa7da05ac899b93
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-02-24 00:00:00 +0000

    initialize Spring Boot project with basic configuration and structure

---
+++ src/main/java/com/fouiguira/pos/inventorypos/InventoryposApplication.java
@@ -0,0 +1,13 @@
+package com.fouiguira.pos.inventorypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fouiguira/pos/inventorypos/InventoryposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fouiguira.pos.inventorypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
