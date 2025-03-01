15d6ff3358c7109465188dedda9b748aca067207
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-03-01 00:52:53 +0000

    feat: add logging to application startup and update version to 0.0.2-SNAPSHOT

--- src/main/java/com/fouiguira/pos/inventorypos/InventoryposApplication.java
+++ src/main/java/com/fouiguira/pos/inventorypos/InventoryposApplication.java
@@ -1,13 +1,21 @@
 package com.fouiguira.pos.inventorypos;
 
 import com.fouiguira.pos.inventorypos.main.MainApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryposApplication {
+    private static final Logger logger = LoggerFactory.getLogger(InventoryposApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Application started.");
+        try {
         MainApp.main(args);
-    }
+        } catch (Exception e) {
+            logger.error("An error occurred: ", e);
 
+        }
+    }
 }
