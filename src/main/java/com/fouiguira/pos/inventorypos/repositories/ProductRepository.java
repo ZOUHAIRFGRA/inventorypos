bdc204a9c9bcd9b462c1ff96066776e5ef7c58b9
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-03-09 15:44:58 +0000

    feat: update repository interfaces to enhance query capabilities; replace Sale and Category references with timestamp and categoryName filters

--- src/main/java/com/fouiguira/pos/inventorypos/repositories/ProductRepository.java
+++ src/main/java/com/fouiguira/pos/inventorypos/repositories/ProductRepository.java
@@ -1,13 +1,11 @@
 package com.fouiguira.pos.inventorypos.repositories;
 
-import com.fouiguira.pos.inventorypos.entities.Category;
 import com.fouiguira.pos.inventorypos.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategory(Category category); // Now expects a Category object
+    List<Product> findByCategoryName(String categoryName);
     List<Product> findByStockQuantityLessThan(int threshold);
 }
--- src/main/java/com/fouiguira/pos/inventorypos/repositories/SaleRepository.java
+++ src/main/java/com/fouiguira/pos/inventorypos/repositories/SaleRepository.java
@@ -1,27 +1,14 @@
 package com.fouiguira.pos.inventorypos.repositories;
 
 import com.fouiguira.pos.inventorypos.entities.Sale;
-import com.fouiguira.pos.inventorypos.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
 import java.util.List;
 
-@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-    List<Sale> findByCashier(User cashier);
-
-    @Query("SELECT COALESCE(SUM(s.totalPrice), 0) FROM Sale s WHERE DATE(s.timestamp) = DATE('now')")
-    Double findTotalSalesToday();
-
-    @Query("SELECT COALESCE(SUM(s.totalPrice), 0) FROM Sale s")
-    Double findTotalRevenue();
-
-    @Query("SELECT COALESCE(SUM(s.totalPrice) - COALESCE(SUM(sp.quantity * sp.product.price), 0), 0) " +
-            "FROM Sale s LEFT JOIN SaleProduct sp ON sp.sale.id = s.id")
-    Double findPendingPayments();
-
-
-    List<Sale> findTop10ByOrderByTimestampDesc();
+    List<Sale> findByTimestampBetween(Date start, Date end);
+    List<Sale> findByOrderByTimestampDesc(PageRequest pageable);
 }
