e420ca1e6b809d9b52d9b6ee0d8f406e188fdeaf
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-03-09 14:44:56 +0000

    refactor: remove unused import for Bean annotation in AppConfig

--- src/main/java/com/fouiguira/pos/inventorypos/config/AppConfig.java
+++ src/main/java/com/fouiguira/pos/inventorypos/config/AppConfig.java
@@ -6,7 +6,6 @@ import com.fouiguira.pos.inventorypos.repositories.SaleRepository;
 import com.fouiguira.pos.inventorypos.repositories.InvoiceRepository;
 import com.fouiguira.pos.inventorypos.services.impl.*;
 import com.fouiguira.pos.inventorypos.services.interfaces.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
