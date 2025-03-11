5a7bd1f002520cbb0ba09a9e086d0a5942e7d89e
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-03-11 11:28:57 +0000

    feat: add new repository methods for filtering invoices and sales by client name and cashier; create SaleProductRepository for sale product management

--- src/main/java/com/fouiguira/pos/inventorypos/repositories/InvoiceRepository.java
+++ src/main/java/com/fouiguira/pos/inventorypos/repositories/InvoiceRepository.java
@@ -1,6 +1,7 @@
 package com.fouiguira.pos.inventorypos.repositories;
 
 import com.fouiguira.pos.inventorypos.entities.Invoice;
+import com.fouiguira.pos.inventorypos.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -9,4 +10,7 @@ import java.util.List;
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     List<Invoice> findByTimestampBetween(LocalDate start, LocalDate end);
     List<Invoice> findByStatus(Invoice.InvoiceStatus status);
+    List<Invoice> findByClientNameContainingIgnoreCase(String clientName);
+    List<Invoice> findByCashier(User cashier);
+    List<Invoice> findBySaleId(Long saleId);
 }
--- src/main/java/com/fouiguira/pos/inventorypos/repositories/SaleRepository.java
+++ src/main/java/com/fouiguira/pos/inventorypos/repositories/SaleRepository.java
@@ -1,14 +1,20 @@
 package com.fouiguira.pos.inventorypos.repositories;
 
 import com.fouiguira.pos.inventorypos.entities.Sale;
+import com.fouiguira.pos.inventorypos.entities.User;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<Sale> findByCashier(User cashier);
+    List<Sale> findByClientNameContainingIgnoreCase(String clientName);
+    List<Sale> findByPaymentMethod(String paymentMethod);
     List<Sale> findByTimestampBetween(Date start, Date end);
     List<Sale> findByOrderByTimestampDesc(PageRequest pageable);
 }
