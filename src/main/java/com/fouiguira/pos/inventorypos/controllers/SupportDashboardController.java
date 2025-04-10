576097a4af43b9b5ef8123ac438916b4f38d0f96
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-04-11 00:57:45 +0100

    Add license headers and update entity classes
    - Added license headers to all entity classes including Category, Invoice, Product, Sale, SaleProduct, and User.
    - Updated the Invoice class to ensure totalAmount, clientName, and cashier are set based on the associated Sale.
    - Added license headers to repository interfaces: BusinessSettingsRepository, CategoryRepository, InvoiceRepository, ProductRepository, SaleProductRepository, SaleRepository, and UserRepository.
    - Added license headers to service interfaces and implementations: BusinessSettingsService, CategoryService, InvoiceService, ProductService, SaleProductService, SalesService, and UserService.
    - Added license headers to utility classes and test files.

--- src/main/java/com/fouiguira/pos/inventorypos/controllers/SupportDashboardController.java
+++ src/main/java/com/fouiguira/pos/inventorypos/controllers/SupportDashboardController.java
@@ -1,3 +1,14 @@
+/*
+ * Inventory POS System
+ * Copyright (c) 2025 ZOUHAIR FOUIGUIRA. All rights reserved.
+ *
+ * Licensed under Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International
+ * You may not use this file except in compliance with the License.
+ *
+ * @author ZOUHAIR FOUIGUIRA
+ * @version 1.0
+ * @since 2025-02-24
+ */
 package com.fouiguira.pos.inventorypos.controllers;
 
 import com.fouiguira.pos.inventorypos.entities.User;
